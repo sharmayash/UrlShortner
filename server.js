6eb892950f697d5c83ec26a6ede409f307624597
yash <sharmayash44ys@gmail.com>
2020-07-25 15:13:38 +0530

    Deploy

--- server.js
+++ server.js
@@ -34,4 +34,11 @@ app.use(passport.initialize())
 
 require("./config/passport")(passport)
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
+
 app.listen(process.env.PORT || 4000, () => console.log("server started"))
