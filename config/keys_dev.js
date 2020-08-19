b55a235115755f346634f06e1da977f2cc527b65
yash <sharmayash44ys@gmail.com>
2020-08-19 23:33:44 +0530

    test4

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: `mongodb://localhost/node-mongo-registration-login-api`,
+  secretOrKey: "THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING",
+}
